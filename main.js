e3e8e1af69724430d78a69f399525d5d0f424dd9
zovcrru <zovcrru@gmail.com>
2020-09-25 17:29:20 +0500

    HomeWork-3

--- main.js
+++ main.js
@@ -1,90 +1,65 @@
-// console.log("%cString", "color:#FFFF00; background-color:#5394EC");
-// console.log("Привет, Марафонец !");
-// console.log(+true);
-// console.log(+null);
-// console.log("null");
-// console.log(null);
-// console.log(!!"");
-// console.log(!!" ");
+const $btn = document.getElementById("btn-kick");
+const $btn_enemy = document.getElementById("btn-kick2");
 
-// console.log("%c&&", "color:#FFFF00; background-color:#5394EC");
-// console.log(1 + 1 === 2 && 10 * 10 > 50);
+const character = {
+  name: "Pickachu",
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById("health-character"),
+  elProgressbar: document.getElementById("progressbar-character"),
+};
 
-// console.log("%c?", "color:#FFFF00; background-color:#5394EC");
-// console.log(true ? 1 : 2);
-// console.log(false ? 1 : 2);
+const enemy = {
+  name: "Charmander",
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById("health-enemy"),
+  elProgressbar: document.getElementById("progressbar-enemy"),
+};
 
-const firstRow = "мама мыла раму";
-const secondRow = "собака друг человека";
-const phone = "+71234567890";
+$btn.addEventListener("click", function () {
+  console.log("Kick");
+  changeHP(random(20), character);
+  changeHP(random(20), enemy);
+});
 
-function countChar(string, char) {
-  let countA = 0;
-  for (let i = 0; i < string.length; i++) {
-    const charString = string.charAt(i);
+$btn_enemy.addEventListener("click", function () {
+  console.log("Kick_enemy");
+  changeHP(random(15), enemy);
+});
 
-    if (charString === char) {
-      countA++;
-    }
-  }
-  return countA;
+function init() {
+  console.log("Start Game !");
+  renderHP(character);
+  renderHP(enemy);
 }
 
-function getRow(firstRow, secondRow) {
-  let countFirstRow, countSecondRow;
-
-  char = prompt("Задай букву !");
-
-  countFirstRow = countChar(firstRow, char);
-  countSecondRow = countChar(secondRow, char);
-  return countFirstRow > countSecondRow ? countFirstRow : countSecondRow;
+function renderHP(person) {
+  renderHPLife(person);
+  renderProgressbarHP(person);
 }
 
-function formattedPhone() {
-  let phone = prompt("Номер телефона: ");
-  let formatPhone = "";
-  if (phone.search(/[а-яёa-z]/) == -1) {
+function renderHPLife(person) {
+  person.elHP.innerText = person.damageHP + "/" + person.defaultHP;
+}
+
+function renderProgressbarHP(person) {
+  person.elProgressbar.style.width = person.damageHP + "%";
+}
+
+function changeHP(count, person) {
+  if (person.damageHP < count) {
+    person.damageHP = 0;
+    alert("Geme over !");
+    $btn.disabled = true;
+    $btn_enemy.disabled = true;
   } else {
-    alert("Формат функции неверный !");
-    return "";
+    person.damageHP -= count;
   }
-  j = 0;
-  for (let i = 0; i < phone.length; i++) {
-    const char = phone.charAt(i);
-    formatPhone += char;
-    if (i === 0) {
-      switch (char) {
-        case "+":
-          j = 1;
-          break;
-        case "8":
-        case "7":
-          formatPhone = "+7 (";
-        case "9":
-          formatPhone = "+7 (9";
-          j = 1;
-          break;
-        default:
-          alert("Формат функции неверный !");
-          break;
-          return "";
-      }
-    }
-    if (i === 1 && char === "7") {
-      formatPhone += " (";
-      j -= 2;
-    }
-    if (j === 3) {
-      formatPhone += ") ";
-    }
-    if (j === 6 || j === 8) {
-      formatPhone += "-";
-    }
-    j++;
-  }
-
-  return formatPhone;
+  renderHP(person);
 }
 
-alert(formattedPhone()); // +7 (123) 456-78-90
-alert(getRow(firstRow, secondRow));
+function random(num) {
+  return Math.ceil(Math.random() * num);
+}
+init();
